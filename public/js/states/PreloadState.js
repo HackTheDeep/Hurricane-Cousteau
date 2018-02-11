128a202f52d6c5fe5434e95c0c804bbd40d297f1
Jackson Sui <kiro705@gmail.com>
2018-02-11 12:01:06 -0500

    end of time

--- public/js/states/PreloadState.js
+++ public/js/states/PreloadState.js
@@ -3,6 +3,7 @@ var PreloadState = {
 	preload: function(){
     this.load.image('hurricane', 'assets/hurricane_circle.png')
     this.load.image('vDrifter', 'assets/orange_drifter.png')
+    this.load.image('rDrifter', 'assets/green_drifter.png')
 	},
 
 	create: function(){
